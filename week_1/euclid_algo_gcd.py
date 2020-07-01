2ef3ce6366075d1c15c23cee09e1bb7fb6e15c22
Dibyanshu Shekhar Dey <66998557+dibyanshushekhardey@users.noreply.github.com>
2020-07-01 19:47:39 +0530

    Add files via upload

---
+++ week_1/euclid_algo_gcd.py
@@ -0,0 +1,11 @@
+def gcd(m, n):
+    if m < n:  # Assume m >= n
+        (m, n) = (n, m)
+    while (m % n) != 0:
+        diff = m - n
+        (m, n) = (max(n, diff), min(n, diff))
+
+    return(n)
+
+
+print(gcd(18, 63))
---
+++ week_1/even_better_gcd.py
@@ -0,0 +1,10 @@
+# no lists
+mrcf = 0
+
+def gcd(m, n):
+    for i in range(1, min(m, n) + 1):
+        if m % i == 0  and n % i == 0:
+            mrcf = i
+    return mrcf
+
+print(gcd(14, 63))
